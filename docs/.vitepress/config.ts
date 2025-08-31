5e4933ece6fe548db50dcd690deea58696ebc808
LukeGus <bugattiguy527@gmail.com>
2025-08-31 00:42:56 -0500

    Update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
           {text: 'Installation', link: '/install'},
           {text: 'OIDC', link: '/oidc'},
           {text: 'Tunnels', link: '/tunnels'},
+          {text: 'Contributing', link: '/contributing'},
           {
             text: "API Reference",
             collapsed: true,
